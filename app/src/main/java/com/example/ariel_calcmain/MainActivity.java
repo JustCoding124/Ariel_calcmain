7b3576aabd778b8426dcec908e51053f43b90f61
JustCoding124 <99827982+JustCoding124@users.noreply.github.com>
2024-04-19 17:33:29 +0300

    2

--- app/src/main/java/com/example/ariel_calcmain/MainActivity.java
+++ app/src/main/java/com/example/ariel_calcmain/MainActivity.java
@@ -11,6 +11,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private double last;
+    private String text;
+    private String sign ="";
+    private double first=0;
+    private double secound=0;
     private EditText display;
     private Button plus;
     private Button minus;
@@ -37,11 +42,135 @@ public class MainActivity extends AppCompatActivity {
         ac =  (Button) findViewById(R.id.ac);
         equal =  (Button) findViewById(R.id.equal);
         credits =  (Button) findViewById(R.id.credits);
+    }
+    public int plus(){
+        text = display.getText().toString();
+        if (!sign.isEmpty()){
+            text = text.substring(1);
+            for (int i =0; i<text.length();i++){
+                if (Integer.valueOf(text.charAt(i))<49 || Integer.valueOf(text.charAt(i))>57 ){
+                    display.setText("ERROR");
+                    return -1;
+                }
+            }
+            secound = Integer.valueOf(text);
+            first +=secound;
+        }
+        else {
+            for (int i = 0; i < text.length(); i++) {
+                if (Integer.valueOf(text.charAt(i)) < 49 || Integer.valueOf(text.charAt(i)) > 57) {
+                    display.setText("ERROR");
+                    return -1;
+                }
+            }
+            first = Integer.valueOf(text);
+        }
+        sign = "+";
+        display.setText("+");
+        return 0;
 
+    }
+    public int minus(){
+        text = display.getText().toString();
+        if (!sign.isEmpty()){
+            text = text.substring(1);
+            for (int i =0; i<text.length();i++){
+                if (Integer.valueOf(text.charAt(i))<49 || Integer.valueOf(text.charAt(i))>57 ){
+                    display.setText("ERROR");
+                    return -1;
+                }
+            }
+            secound = Integer.valueOf(text);
+            first -=secound;
+        }
+        else {
+            for (int i = 0; i < text.length(); i++) {
+                if (Integer.valueOf(text.charAt(i)) < 49 || Integer.valueOf(text.charAt(i)) > 57) {
+                    display.setText("ERROR");
+                    return -1;
+                }
+            }
+            first = Integer.valueOf(text);
+        }
+        sign = "-";
+        display.setText("-");
+        return 0;
+    }
+    public int multiply(){
+        text = display.getText().toString();
+        if (!sign.isEmpty()){
+            text = text.substring(1);
+            for (int i =0; i<text.length();i++){
+                if (Integer.valueOf(text.charAt(i))<49 || Integer.valueOf(text.charAt(i))>57 ){
+                    display.setText("ERROR");
+                    return -1;
+                }
+            }
+            secound = Integer.valueOf(text);
+            first *=secound;
+        }
+        else {
+            for (int i = 0; i < text.length(); i++) {
+                if (Integer.valueOf(text.charAt(i)) < 49 || Integer.valueOf(text.charAt(i)) > 57) {
+                    display.setText("ERROR");
+                    return -1;
+                }
+            }
+            first = Integer.valueOf(text);
+        }
+        sign = "*";
+        display.setText("*");
+        return 0;
+    }
+    public int divide(){
+        text = display.getText().toString();
+        if (!sign.isEmpty()){
+            text = text.substring(1);
+            for (int i =0; i<text.length();i++){
+                if (Integer.valueOf(text.charAt(i))<49 || Integer.valueOf(text.charAt(i))>57 ){
+                    display.setText("ERROR");
+                    return -1;
+                }
+            }
+            secound = Integer.valueOf(text);
+            first /=secound;
+        }
+        else {
+            for (int i = 0; i < text.length(); i++) {
+                if (Integer.valueOf(text.charAt(i)) < 49 || Integer.valueOf(text.charAt(i)) > 57) {
+                    display.setText("ERROR");
+                    return -1;
+                }
+            }
+            first = Integer.valueOf(text);
+        }
+        sign = "/";
+        display.setText("/");
+        return 0;
+    }
+    public void equal(){
+        if (sign.isEmpty()){display.setText("ERROR");}
 
-
-
-
+        else{
+            text = display.getText().toString();
+            secound=Integer.valueOf(text.substring(1));
+            switch (sign){
+                case "+":
+                    last = first+secound;
+                    display.setText(""+last);
+                case "-":
+                    last = first-secound;
+                    display.setText(""+last);
+                case "*":
+                    last = first*secound;
+                    display.setText(""+last);
+                case "/":
+                    last = first/secound;display.setText(""+last); default: display.setText("ERROR");
+            }
+            sign = "";
+        }
+    }
+    public void credits(){
 
     }
 }
